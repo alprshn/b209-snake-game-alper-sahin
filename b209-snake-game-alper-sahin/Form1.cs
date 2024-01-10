278e4e4e28cab1e939355e6194d22fd20e92b318
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-01-10 14:43:43 +0100

    Added

---
+++ b209-snake-game-alper-sahin/Form1.cs
@@ -0,0 +1,10 @@
+namespace b209_snake_game_alper_sahin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ b209-snake-game-alper-sahin/Program.cs
@@ -0,0 +1,17 @@
+namespace b209_snake_game_alper_sahin
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
